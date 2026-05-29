0a631670b81407082dab3d5657434b7b4dc15c7b
saranya <saranyanekkanti165@gmail.com>
2026-05-29 20:13:53 +0530

    Made portfolio responsive

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
 var typed = new Typed(".text", {
     strings: ["Aspiring Full Stack Developer",
         
