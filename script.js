1143f38323f09f39125297aba87adefcb06f1525
saranya <saranyanekkanti165@gmail.com>
2026-05-29 19:13:51 +0530

    MyPersonalPortfolio Website

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var typed = new Typed(".text", {
+    strings: ["Aspiring Full Stack Developer",
+        
+        "Aspiring Frontend Web Developer",
+        "Spring Boot Learner"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 500,
+    loop: true
+});
